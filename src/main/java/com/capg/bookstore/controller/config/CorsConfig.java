443bdc260da1a16e34091a34f7fee11aeb14988a
RevanthLavanur <108509657+RevanthLavanur@users.noreply.github.com>
2024-03-27 18:54:46 +0530

    Update CorsConfig.java

--- src/main/java/com/capg/bookstore/controller/config/CorsConfig.java
+++ src/main/java/com/capg/bookstore/controller/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:3000") // Replace this with your frontend URL
+            .allowedOrigins("https://front-end-bookstore.onrender.com") // Replace this with your frontend URL
             .allowedMethods("GET", "POST", "PUT", "DELETE")
             .allowCredentials(true)
               .allowedHeaders("*");
